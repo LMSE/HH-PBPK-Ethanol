ac7d0525e697632cd565c1935fc50d3e2625cafd
leozhu1996 <43071853+leozhu1996@users.noreply.github.com>
2021-04-07 16:49:00 -0400

    Delete AverageError.m

--- AverageError.m
+++
@@ -1,9 +0,0 @@
-function MAE = AverageError (time, expdata, modeldata, it)
-    n=length(time);
-    time = time*it;
-    MAE = 0;
-    for i = 1:n
-        MAE = MAE+ abs (expdata(i) - modeldata(time(i)+1));
-    end
-    MAE = MAE/n;
-end
